7d19d1a835198f0a3ab4ae236c796a71419d10c5
Kei Hibino <ex8k.hibino@gmail.com>
2022-05-21 19:36:47 +0900

    mark5gc: add tak example.

--- TemplateMark5GC.hs
+++ TemplateMark5GC.hs
@@ -871,6 +871,10 @@ testDouble4 =
   "double x = x + x ;\n\
   \main = double (double (S K K 3))"
 
+testTak =
+  "tak x y z = if (y < x) (tak (tak (x-1) y z) (tak (y-1) z x) (tak (z-1) x y)) y ;\n\
+  \main = tak 12 6 0"
+
 testNeg = "main = negate 3"
 testNeg2 = "main = negate (negate 3)"
 
