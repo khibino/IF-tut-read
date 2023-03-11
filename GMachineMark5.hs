62cade2c49d22891ed9ce879891c0e92975aff1f
Kei Hibino <ex8k.hibino@gmail.com>
2023-03-11 21:41:08 +0900

    gm-mark5: add note about compilation rule

--- GMachineMark5.hs
+++ GMachineMark5.hs
@@ -399,7 +399,7 @@ rule 3.23 からすると、 (空命令列, 空スタック) が dump に積ま�
 
 compileSc :: (Name, [Name], CoreExpr) -> GmCompiledSC
 compileSc (name, env, body) =
-  (name, length env, compileR body $ zip env [0..])
+  (name, length env, compileR body $ zip env [0..])  {- Fig 3.3  p.100 -}
 
 type GmEnvironment = Assoc Name Int
 type GmCompiler = CoreExpr -> GmEnvironment -> GmCode
@@ -408,38 +408,38 @@ compileRslide :: GmCompiler
 compileRslide e env = compileC e env ++ [Slide (length env + 1), Unwind]
 
 compileR :: GmCompiler
-compileR e env = compileE e env ++ [Update n, Pop n, Unwind] {- exercise 3.28 -}
+compileR e env = compileE e env ++ [Update n, Pop n, Unwind]  {- Fig 3.12  p.127 -} {- exercise 3.28 -}
   where n = length env
 
 -- exercise 3.28
 compileE :: GmCompiler
-compileE (ENum n) _env =  [Pushint n]
+compileE (ENum n) _env =  [Pushint n]  {- Fig 3.12  p.127 -}
 compileE (ELet recursive defs e) env
-  | recursive  = compileLetrec compileE defs e env
-  | otherwise  = compileLet    compileE defs e env
+  | recursive  = compileLetrec compileE defs e env  {- Fig 3.12  p.127 -}
+  | otherwise  = compileLet    compileE defs e env  {- Fig 3.12  p.127 -}
 compileE (EAp (EAp (EVar opn) e0) e1) env
-  | Just op <- lookup opn builtInDyadic = compileE e1 env ++ compileE e0 env ++ [op]
-compileE (EAp (EVar "negate") e) env = compileE e env ++ [Neg]
+  | Just op <- lookup opn builtInDyadic = compileE e1 env ++ compileE e0 env ++ [op]  {- Fig 3.12  p.127 -}
+compileE (EAp (EVar "negate") e) env = compileE e env ++ [Neg]  {- Fig 3.12  p.127 -}
 compileE (EAp (EAp (EAp (EVar "if") e0) e1) e2) env =
-  compileE e0 env ++ [Cond (compileE e1 env) (compileE e2 env)]
-compileE e env = compileC e env ++ [Eval]
+  compileE e0 env ++ [Cond (compileE e1 env) (compileE e2 env)]  {- Fig 3.12  p.127 -}
+compileE e env = compileC e env ++ [Eval]  {- Fig 3.12  p.127 -}
 
 compileC :: GmCompiler
 compileC (EVar v)     env
-  | v `elem` (aDomain env)  =  [Push n]
-  | otherwise               =  [Pushglobal v]
+  | v `elem` (aDomain env)  =  [Push n]              {- Fig 3.10  p.114, Fig 3.3  p.100 -}
+  | otherwise               =  [Pushglobal v]        {- Fig 3.10  p.114, Fig 3.3  p.100 -}
   where n = aLookup env v (error "compileC.EVar: Can't happen")
-compileC (ENum n)     env   =  [Pushint n]
+compileC (ENum n)     env   =  [Pushint n]           {- Fig 3.10  p.114, Fig 3.3  p.100 -}
 compileC (EAp e1 e2)  env   =  compileC e2 env ++
                                compileC e1 (argOffset 1 env) ++
-                               [Mkap]
+                               [Mkap]                {- Fig 3.10  p.114, Fig 3.3  p.100 -}
 compileC (ELet recursive defs e) env
   | recursive  = compileLetrec compileC defs e env
   | otherwise  = compileLet    compileC defs e env
 
 compileLet :: GmCompiler -> [(Name, CoreExpr)] -> GmCompiler
 compileLet comp defs expr env =
-  compileLet' defs env ++ comp expr env' ++ [Slide (length defs)]
+  compileLet' defs env ++ comp expr env' ++ [Slide (length defs)]  {- Fig 3.10  p.114 -}
   where env' = compileArgs defs env
 
 compileLet' :: [(Name, CoreExpr)] -> GmEnvironment -> GmCode
@@ -455,7 +455,7 @@ compileArgs defs env =
 -- exercise 3.16
 compileLetrec :: GmCompiler -> [(Name, CoreExpr)] -> GmCompiler
 compileLetrec  comp defs expr env =
-  [Alloc n] ++ compileLetrec' defs env' ++ comp expr env' ++ [Slide n]
+  [Alloc n] ++ compileLetrec' defs env' ++ comp expr env' ++ [Slide n]  {- Fig 3.10  p.114 -}
   where
     env' = compileArgs defs env
     n = length defs
