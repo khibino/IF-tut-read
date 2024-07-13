50465d07aebeda5d4c8d3602e72195abab39d0d9
Kei Hibino <ex8k.hibino@gmail.com>
2024-07-14 04:08:26 +0900

    tim-mark4: add pprCompile

--- TimMark4.hs
+++ TimMark4.hs
@@ -894,6 +894,9 @@ nTerse = 3
 
 ---
 
+pprCompile :: CoreProgram -> IO ()
+pprCompile = putStr . iDisplay . showSCDefns . compile
+
 test' :: Bool -> String -> IO ()
 test' doGC = putStr . fullRun' doGC
 
