2853320bbb2d534f626449a9a25beb5989dcdf15
Kei Hibino <ex8k.hibino@gmail.com>
2025-02-15 23:44:24 +0900

    pgm-mark1: update signature, compile

--- PgmMark1.hs
+++ PgmMark1.hs
@@ -549,10 +549,10 @@ builtInDyadic =
 
 -- Compiling a program
 
-compile :: CoreProgram -> GmState
+compile :: CoreProgram -> PgmState
 compile program =
-  undefined
-  -- ([], initialCode, Stack [] 0 0, Stack [] 0 0, [], heap, globals, statInitial)
+  (([], heap, globals, [], []),
+   [(initialCode, Stack [] 0 0, Stack [] 0 0, [], 0)])
   where (heap, globals) = buildInitialHeap program
 
 buildInitialHeap :: CoreProgram -> (GmHeap, GmGlobals)
@@ -727,19 +727,19 @@ skk3 = [ ("main", [], ap3 (EVar "S") (EVar "K") (EVar "K") (ENum 3)) ]
   where ap2 f x y = EAp (EAp f x) y
         ap3 f x y z = EAp (ap2 f x y) z
 
-test1 :: GmState
+test1 :: PgmState
 test1 = compile (skk3 ++ preludeDefs)
 
 ---
 
 run :: String -> String
-run = showResults_ . eval_ . compile . parse
+run = showResults . eval . compile . parse
 
-evalList :: GmState -> [GmState]
+evalList :: PgmState -> [PgmState]
 evalList = undefined
 
-compileList :: CoreProgram -> GmState
-compileList program = undefined
+compileList :: CoreProgram -> PgmState
+compileList _program = undefined
 
 extraPreludeDefs :: CoreProgram
 extraPreludeDefs =
@@ -773,9 +773,6 @@ extraPreludeDefs =
   , ("printCons",["h","t"],EAp (EAp (EVar "print") (EVar "h")) (EAp (EVar "printList") (EVar "t")))
    -}
   ]
-
-showResults_ :: [GmState] -> String
-showResults_ = undefined
 
 showResults :: [PgmState] -> String
 showResults states =
@@ -1141,7 +1138,7 @@ bug_mark6_unwind0_A = "main = I I 3"
 bug_mark6_unwind0_B = testB32nfib
 
 test_ :: Bool -> String -> IO ()
-test_ nestedDebug = putStrLn . showResults_ . eval_ . compile . parse
+test_ _nestedDebug = putStrLn . showResults . eval . compile . parse
 -- test_ nestedDebug = putStrLn . showResults . eval . setDebug . compile . parse
 --   where setDebug = applyToStats (tiStatSetDebugNested nestedDebug)
 
@@ -1152,7 +1149,7 @@ qtest :: String -> IO ()
 qtest = test_ False
 
 testList :: String -> IO ()
-testList = putStrLn . showResults_ . evalList . compileList . parse
+testList = putStrLn . showResults . evalList . compileList . parse
 
 check :: Node -> String -> Either String String
 check expect prog
@@ -1160,7 +1157,7 @@ check expect prog
   | lastv == expect         =  Right . unlines $ showProg "pass: " ++ [show lastv]
   | otherwise               =  Left  . unlines $ ("expect " ++ show expect) : showProg "wrong: "
   where
-    states = take limit . eval_ . compile . parse $ prog
+    states = take limit . eval . compile . parse $ prog
     limit = 1000000
     (   _o, _i, lastStack, _d, _vs, lHeap, _, _) = undefined -- last states
     (a, _) = stkPop lastStack
