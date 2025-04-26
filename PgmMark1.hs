b665edaa1f4dd7e5e2b3ee78bf03842e56842cf6
Kei Hibino <ex8k.hibino@gmail.com>
2025-04-27 02:55:22 +0900

    pgm-mark1: fix NGlobal patterns for unwind

--- PgmMark1.hs
+++ PgmMark1.hs
@@ -428,14 +428,13 @@ unwind state =
             where ((i',s'), dump') = stkPop dump
         newState (NAp a1 _a2) = putCode [Unwind] (putStack (a1<:>a<:>as) state)
         newState (NGlobal n c)
-          | k < n
-          , (ak, _) <- stkPop $ discard k $ getStack state
-          , ((i,s), dump') <- stkPop dump
-                           =  putCode i $ putStack (stkPush ak s) $ putDump dump' state  {- rule 3.29 -}  {- exercise 3.29 -}
-          | otherwise      =  putCode c $ putStack rstack state  {- rule 3.19, updated from rule 3.12 -}
-          where k = depth as
+          | k < n             =  putCode i $ putStack (stkPush ak s) $ putDump dump' state  {- rule 3.29 -}  {- exercise 3.29 -}
+          | otherwise         =  putCode c $ putStack rstack state  {- rule 3.19, updated from rule 3.12 -}
+          where ((i,s), dump') = stkPop dump
+                (ak, _) = stkPop $ discard k $ getStack state
+                k = depth as
                 rstack = rearrange n (getHeap state) $ getStack state  {- exercise 3.12 -}
-        newState (NInd a1) =  putCode [Unwind] (putStack (a1<:>as) state)
+        newState (NInd a1)    =  putCode [Unwind] (putStack (a1<:>as) state)
         newState (NConstr _n _as) = putCode i' $ putStack (a<:>s') $ putDump dump' state  {- rule 3.35 -}
           where ((i',s'), dump') = stkPop dump
         -- newState  n        =  error $ "unwind.newState: unknown node: " ++ show n
