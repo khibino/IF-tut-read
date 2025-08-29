68d25eb9b127374359c563822c43b67e7cf2d6ce
Kei Hibino <ex8k.hibino@gmail.com>
2025-08-29 12:38:36 +0900

    pgm-mark2: apply field selector to unwind

--- PgmMark2.hs
+++ PgmMark2.hs
@@ -490,9 +490,9 @@ unwind state =
           | null (list dump)  = state  {- rule 3.10 -}
           | otherwise         = putCode i' $ putStack (stkPush a s') $ putDump dump' $ state {- rule 3.22-} {- TODO: save maxDepth of as -}
             where ((i',s'), dump') = stkPop dump
-        newState (NAp a1 _a2) = putCode [Unwind] (setRunning $ lock a (putStack (a1<:>a<:>as) state))  {- rule 5.2 -}  {- exercise 5.9 -}
-        newState (NLAp {})    = putCode [Unwind] (setWaiting   state)                                  {- wait NLAp -}  {- exercise 5.9 -}
-        newState (NGlobal n c)
+        newState (NAp{nodeCar=a1}) = putCode [Unwind] (setRunning $ lock a (putStack (a1<:>a<:>as) state))  {- rule 5.2 -}  {- exercise 5.9 -}
+        newState (NLAp{})          = putCode [Unwind] (setWaiting   state)                                  {- wait NLAp -}  {- exercise 5.9 -}
+        newState (NGlobal{nodeArity=n, nodeCode=c})
           | n == 0            =  putCode c $ lock a (setRunning state)  {- rule 5.3, exercise 5.9 -}
           | k < n             =  putCode i $ putStack (stkPush ak s) $ putDump dump' state  {- rule 3.29 -}  {- exercise 3.29 -}
           | otherwise         =  putCode c $ putStack rstack state  {- rule 3.19, updated from rule 3.12 -}
@@ -500,7 +500,7 @@ unwind state =
                 (ak, _) = stkPop $ discard k $ getStack state
                 k = depth as
                 rstack = rearrange n (getHeap state) $ getStack state  {- exercise 3.12 -}
-        newState (NLGlobal _ _n _c)
+        newState (NLGlobal{})
                               =  putCode [Unwind]   (setWaiting state)  {- wait NLGlobal, exercise 5.9 -}
         newState (NInd a1)    =  putCode [Unwind] (putStack (a1<:>as) state)
         newState (NConstr _n _as) = putCode i' $ putStack (a<:>s') $ putDump dump' state  {- rule 3.35 -}
