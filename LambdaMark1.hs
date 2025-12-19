4933466805655cc36d088d7f9e5ff922e296443c
Kei Hibino <ex8k.hibino@gmail.com>
2025-12-20 00:17:34 +0900

    lambda-mark1: signatures - 6.3.1

--- LambdaMark1.hs
+++ LambdaMark1.hs
@@ -1,5 +1,28 @@
 
 module LambdaMark1 where
 
+import Data.Set (Set)
+import qualified Data.Set as Set
+
 import Language
 import Utils
+
+-----
+
+freeVars :: CoreProgram -> AnnProgram Name (Set Name)
+freeVars = _
+
+abstract :: AnnProgram Name (Set Name) -> CoreProgram
+abstract = _
+
+rename :: CoreProgram -> CoreProgram
+rename = _
+
+collectSCs :: CoreProgram -> CoreProgram
+collectSCs = _
+
+lambdaLift :: CoreProgram -> CoreProgram
+lambdaLift = collectSCs . rename . abstract . freeVars
+
+runS :: String -> String
+runS = pprint . lambdaLift . parse
